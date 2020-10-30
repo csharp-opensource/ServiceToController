1c517c9cdc3b508705fff7f36a55758a0756214b
Bar Nuri <barn@ke-la.com>
2020-10-30 22:27:02 +0200

    add publish script

---
+++ buildAndPublish.ps1
@@ -0,0 +1,24 @@
+#Get Path to csproj
+$path = ".\ServiceToController.csproj"
+
+#Read csproj (XML)
+$xml = [xml](Get-Content $path)
+
+#Split the Version Numbers
+$avMajor, $avMinor, $avBuild  = $xml.Project.PropertyGroup.Version.Split(".")
+
+#Increment
+$avBuild = [Convert]::ToInt32($avBuild,10) + 1
+
+#Put new version back into csproj (XML)
+$version = "$avMajor.$avMinor.$avBuild"
+$xml.Project.PropertyGroup.Version = $version
+
+#Save csproj (XML)
+$xml.Save($path)
+
+Remove-Item './pack' -Recurse
+dotnet pack -c Release -o ./pack
+dotnet nuget push "./pack/ServiceToController.$version.nupkg" --api-key $env:NugetApiKey --source https://api.nuget.org/v3/index.json
+
+
